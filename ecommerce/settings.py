e689c4504d80148e7eb5101ff9d9980c3b8d513f
sabermhareshdj <dj.saber.mharesh@gmail.com>
2024-05-12 10:51:24 +0300

    l19 Configuring Django for image uploads

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -122,6 +122,12 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = [BASE_DIR / 'static/media']
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
